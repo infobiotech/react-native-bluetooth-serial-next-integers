da464d68f22b3417cc3844a100bc2bc96feb63ad
a.raffa@infobiotech.com <a.raffa@infobiotech.com>
2019-11-07 18:33:57 +0100

    fix Exception name error

--- android/src/main/java/com/nuttawutmalee/RCTBluetoothSerial/RCTBluetoothSerialService.java
+++ android/src/main/java/com/nuttawutmalee/RCTBluetoothSerial/RCTBluetoothSerialService.java
@@ -444,7 +444,7 @@ class RCTBluetoothSerialService {
                      */
                     if (bytes != messageLength) {
                       Log.e(TAG, "data length mismatch");
-                      mModule.onError(new Exeption("data length mismatch"));
+                      mModule.onError(new Exception("data length mismatch"));
                     }
                     else {
                       /*
